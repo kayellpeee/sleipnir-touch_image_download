836314ef9daf433fafb5fb23484bbef8d05e7862
Keenan Lidral-Porter <klp2012@gmail.com>
2014-12-18 10:07:00 -0800

    adds meta data and hello world

---
+++ touch_download.slex.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name            touch_download
+// @namespace       http://fenrir-inc.com
+// @author          Keenan_L-P
+// @description     Touch any image with 2 fingers to open in a new tab
+// @description     Makes images easier to download
+// @include         http://*
+// @version         0.1
+// ==/UserScript==
+ 
+
+alert('hello world');
