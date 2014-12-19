cab5257ca5944be341b4cf2f09fb3cbd993418ea
Keenan Lidral-Porter <klp2012@gmail.com>
2014-12-19 14:20:26 -0800

    updates commented meta data

--- touch_download.slex.js
+++ touch_download.slex.js
@@ -1,11 +1,11 @@
 // ==UserScript==
-// @name            touch_download
+// @name            touch_image_download
 // @namespace       http://fenrir-inc.com
-// @author          Keenan_L-P
+// @author          Keenan_Lidral-Porter
 // @description     Touch any image with 2 fingers to open in a new tab
 // @description     Makes images easier to download
 // @include         http://*
-// @version         0.1
+// @version         1.0
 // ==/UserScript==
  
 var images = window.document.getElementsByTagName('img');
