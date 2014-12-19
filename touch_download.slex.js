7b4b9cba911ad451faee82b923113a5989bc4ad6
Keenan Lidral-Porter <klp2012@gmail.com>
2014-12-19 15:16:49 -0800

    updates meta data for posting to extensions gallery

--- touch_download.slex.js
+++ touch_download.slex.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name            touch_image_download
 // @namespace       http://fenrir-inc.com
-// @author          Keenan_Lidral-Porter
-// @description     Touch any image with 2 fingers to open in a new tab
+// @author          Keenan Lidral-Porter
+// @description     Double tap any image to open in a new tab
 // @description     Makes images easier to download
 // @include         http://*
 // @version         1.0
@@ -14,13 +14,15 @@ Array.prototype.forEach.call(images, function(node){
   var tapped = false;
   node.addEventListener("touchstart", function(evt){
     var touches = evt.touches;
-    if( tapped ){
-      window.open(this.src);
-    }else{
-      tapped = true;
-      setTimeout(function(){
-        tapped = false;
-      }, 500);
+    if( touches.length === 1 ){
+      if( tapped ){
+        window.open(this.src);
+      }else{
+        tapped = true;
+        setTimeout(function(){
+          tapped = false;
+        }, 500);
+      }
     }
   }, false);
 });
